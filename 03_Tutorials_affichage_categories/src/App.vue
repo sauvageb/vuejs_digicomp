f9c530d3a6efad17cc1d12bc66002c0f6db8980f
sauvageb <sauvageboris.pro@gmail.com>
2024-08-15 13:51:16 +0200

    exercice 3

---
+++ 03_Tutorials_affichage_categories/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import TutorialList from "@/views/TutorialList.vue";
+import CategoryList from "@/views/CategoryList.vue";
+</script>
+
+<template>
+  <main>
+    <TutorialList/>
+    <CategoryList/>
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ 03_Tutorials_affichage_categories/src/views/TutorialList.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>Tutorials component</div>
+</template>
+
+<style scoped>
+
+</style>
